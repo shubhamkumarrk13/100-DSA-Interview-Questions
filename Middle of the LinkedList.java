3faed0219dedfeadb98531a63103e5d7fe120f57
shubhamkumarrk13 <13ishubhamkumar@gmail.com>
2025-08-29 21:19:58 +0530

    Create Middle of the LinkedList.java

---
+++ Middle of the LinkedList.java
@@ -0,0 +1,20 @@
+// Problem Link : https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+class Solution {
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+
+            slow = slow.next;
+
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+}
