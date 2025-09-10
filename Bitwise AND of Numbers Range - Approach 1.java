2c809bc1df0f7dd16f0404f4a9542e7d00195991
shubhamkumarrk13 <13ishubhamkumar@gmail.com>
2025-09-10 09:29:50 +0530

    Create Bitwise AND of Numbers Range - Approach 1.java

---
+++ Bitwise AND of Numbers Range - Approach 1.java
@@ -0,0 +1,20 @@
+// Problem Link : https://leetcode.com/problems/bitwise-and-of-numbers-range/
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+
+        int shifts = 0;
+
+        while(left != right) {
+
+            left = left >> 1;
+
+            right = right >> 1;
+
+            shifts++;
+        }
+
+        return left << shifts;
+        
+    }
+}
